90eeecd7ec563eaee0c914946ca68cd12069c282
myng15 <nguyenhanhhuyenmy@gmail.com>
2025-09-24 01:28:35 +0200

    add anonymizer (generative) and classifier models

---
+++ models/linear.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+class LinearLayer(nn.Module):
+    def __init__(self, input_dimension, num_classes, bias=True):
+        super(LinearLayer, self).__init__()
+        self.input_dimension = input_dimension
+        self.num_classes = num_classes
+        self.fc = nn.Linear(input_dimension, num_classes, bias=bias)
+
+    def forward(self, x):
+        return self.fc(x)
+
